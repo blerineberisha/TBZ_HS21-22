ab1d624745539aaf25c95531e5c076789b12ebcd
blerineberisha <blerine.berisha@noseryoung.ch>
2021-10-05 09:49:22 +0200

    -

--- TBZ/M226/HashMaps/src/com/company/Responder.java
+++ TBZ/M226/HashMaps/src/com/company/Responder.java
@@ -1,31 +1,32 @@
 package com.company;
 
 import java.util.HashMap;
+import java.util.Scanner;
 
 public class Responder {
     InputReader ir = new InputReader();
+    Scanner scan = new Scanner(System.in);
     HashMap responseMap = new HashMap();
-    private void fillResponseMap(){
+
+    private void fillResponseMap() {
         responseMap.put("slow", "I think this has to do with your hardware");
         responseMap.put("bug", "All software has bugs. That's life, deal with it.");
         responseMap.put("crash", "Have you tried turning it off and on again?");
     }
 
-    private void printResponse(){
+    private void printResponse() {
         System.out.println("Please describe your problem.");
         String[] input = ir.readInput();
-
-        for(int i=0; i < input.length; i++)
-        if(responseMap.containsKey(input[i])){
-            System.out.println(responseMap.get(input[i]));
-        }
-        else{
-            System.out.println("I don't know either man.");
-            break;
-        }
+        for (int i = 0; i < input.length; i++)
+            if (responseMap.containsKey(input[i])) {
+                System.out.println(responseMap.get(input[i]));
+            } else {
+                System.out.println("I don't know either man.");
+                break;
+            }
     }
 
-    public void startSupport(){
+    public void startSupport() {
         fillResponseMap();
         printResponse();
     }
