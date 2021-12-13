62a5e5f19d89b88383ee1efb715b8474eca38273
blerineberisha <blerine.berisha@noseryoung.ch>
2021-12-13 14:57:15 +0100

    base for project

---
+++ TBZ/M226/StockExchange/src/com/company/StockTokyo.java
@@ -0,0 +1,14 @@
+package com.company;
+
+/**
+ * -----------------------------------------------------
+ * Author:          Blerinë Berisha
+ * Date:            11.12.2021
+ * Project:         stock exchange
+ * Description:     a program that simulates a stock
+ *                  exchange
+ * ------------------------------------------------------
+ **/
+public class StockTokyo {
+    
+}
